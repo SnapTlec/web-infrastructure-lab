aa129dda063167a2aaf5353d71c8db87bdf4f99f
Leonardo Brito <leonardobrito371@gmail.com>
2026-06-01 19:58:42 -0300

    Ajuste dos xml master e slave

--- wildfly/Start.sh
+++ wildfly/Start.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 
 echo "Iniciando Wildfly"
-echo "ROLE=${ROLE}"
 
-if [ "ROLE"= "controller" ]; then
-    echo "Inicializando a instância controller"
 
-    exec /opt/wildfly/bin/domain.sh \
-            -b 0.0.0.0 \ 
-            -bmanagement 0.0.0.0 \
-            --host-config=host-master.xml
+if ! grep -q "${MGMT_USER}" teste.txt; then
 
-elif [ "ROLE" = "slave" ]; then
-
-    echo "Inicializando a instância slave"
-
-    exec /opt/wildfly/bin/domain.sh \
-            -b 0.0.0.0 \ 
-            -bmanagement 0.0.0.0 \
-            --host-config=host-slave.xml
-else
-    echo "ROLE inválida"
-
-    exit 1
+    exec /opt/jboss/wildfly/bin/add-user.sh \
+        -u "${MGMT_USER}" \
+        -p "${MGMT_PASSWD}" \
+        --silent
 fi
+
+exec /opt/jboss/wildfly/bin/domain.sh \
+    -b 0.0.0.0 \
+    -bmanagement 0.0.0.0 \
+    --host-config="${HOST_CONFIG}"
+
