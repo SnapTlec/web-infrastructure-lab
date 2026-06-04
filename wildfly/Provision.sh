875fe756b3d5d8e1f10b00c79bf6ef1a9f516ea8
Leonardo Brito <leonardobrito371@gmail.com>
2026-06-03 22:04:37 -0300

    Adição da configuração do pool e das validações de conexão do banco de dados

--- wildfly/Provision.sh
+++ wildfly/Provision.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
+#   Interface
+#       |
+#       v
+#   Socket-Binding
+#       |
+#       v
+#   Profiles
+#       |
+#       v
+#   Server-group
+#       |
+#       v
+#   Server
 
 ## Configurar o módulo JDBC
 /profile=full/subsystem=datasources/jdbc-driver=SQLServer:add(driver-module-name=com.microsoft.sqlserver)
 
 ## Configurar o módulo database
-data-source --profile=full add --name=MyDS --jndi-name=java:/jdbc/MyDS --driver-name=SQLServer --connection-url=jdbc:sqlserver://localhost:1433;databaseName=master;encrypt=true;trustServerCertificate=true --user-name=sa --password=SenhaForte@123 --use-ccm=false --max-pool-size=10 --enabled=true
+data-source --profile=full add \
+            --name=MyDS \
+            --jndi-name=java:/jdbc/MyDS \
+            --driver-name=SQLServer \
+            --connection-url=jdbc:sqlserver://localhost:1433;databaseName=master;encrypt=true;trustServerCertificate=true \
+            --enabled=true
 
 ## Configurar o módulo connection pool
+data-source --profile=full \
+            --name=MyDS \
+            --idle-timeout-minutes=1 \
+            --max-pool-size=10 \
+            --min-pool-size=3 \
+            --pool-prefill=true \
+            --pool-use-strict-min=true \
+            --use-ccm=true \
+            --background-validation=true \
+            --flush-strategy=FailingConnectionOnly \
+            --user-name=sa \
+            --password=SenhaForte@123 \
 
 
 ## Configurar o módulo teste de conectividade
+ /host=primary/server=teste/subsystem=datasources/data-source=MyDS:test-connection-in-pool
